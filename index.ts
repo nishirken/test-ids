20b8e5693158ffc0e2013dad91f22b124d755c67
Скурихин Дмитрий <d.skurihin@corp.mail.ru>
2022-04-16 20:18:10 +0400

    add selectors

--- index.ts
+++ index.ts
@@ -6,3 +6,20 @@ type TestIdValue = StaticId | DynamicId;
 export type TestIds = Record<string, TestIdValue>;
 
 export const makeTestIdSelectors = () => {};
+export const makeTestIdSelectors = <
+  S extends Record<string, any> = StringResult<string> & CreatorsResult<string>
+>(
+  testIds: S,
+): S => {
+  const selectors: any = {};
+
+  for (const key in testIds) {
+    const value: any = testIds[key];
+    selectors[key] =
+      typeof value === 'string'
+        ? testIdSelector(value)
+        : (x: number | string) => testIdSelector(value(x));
+  }
+
+  return selectors;
+};
