df0923cedb32723c8c1a395449b4a12470e93bc0
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2022-04-17 13:47:43 +0400

    complete

--- index.test.ts
+++ index.test.ts
@@ -1,13 +1,16 @@
-import {TestIds, makeTestIdSelectors} from "./";
+import {JSDOM} from 'jsdom';
+import {makeTestIdSelectors, withAttrs, testIdPropString, testIdProp} from './index';
 
 type ListItem = {
   id: string;
   name: string;
 };
 
-const testIds: TestIds = {
+const testIds = {
   list: 'list',
+  listWithAttrs: withAttrs<{name: string}>('list'),
   listItem: (item: ListItem) => `list-item-${item.id}`,
+  listItemWithAttrs: (item: ListItem) => withAttrs<{status: 'A' | 'B'}>(`list-item-${item.id}`),
 };
 
 const testIdSelectors = makeTestIdSelectors(testIds);
@@ -28,8 +31,50 @@ describe('Test', () => {
     expect(testIdSelectors.list).toBe('[data-test-id="list"]');
   });
 
-
   test('dynamic field', () => {
     expect(testIdSelectors.listItem(items[0])).toBe('[data-test-id="list-item-1"]');
+  });
+
+  test('With attrs', () => {
+    expect(testIdSelectors.listItemWithAttrs(items[0]).attributes({status: 'A'})).toBe('[data-test-id="list-item-1"][data-status="A"]');
+  });
+
+  test('testIdProp', () => {
+    expect(testIdProp(testIds.list, {name: 'A'})).toEqual({
+      'data-test-id': testIds.list,
+      'data-name': 'A',
+    });
+  });
+
+  test('testIdPropString', () => {
+    expect(testIdPropString(testIds.listItem(items[0]), {status: 'A'})).toBe(`data-test-id="${testIds.listItem(items[0])}" data-status="A"`);
+  });
+
+  test('jsdom', () => {
+    const dom = new JSDOM(`
+      <ul ${testIdPropString(testIds.list)}>
+        ${items.map((item) => `<li ${testIdPropString(testIds.listItem(item))}>${item.name}</li>`)}
+      </ul>
+    `);
+    const ul = dom.window.document.querySelector(testIdSelectors.list);
+    const li1 = dom.window.document.querySelector<HTMLLIElement>(testIdSelectors.listItem(items[0]));
+
+    expect(ul).not.toBeNull();
+    expect(li1?.innerHTML).toBe(items[0].name);
+  });
+
+  test('jsdom with attributes', () => {
+    const dom = new JSDOM(`
+      <ul ${testIdPropString(testIds.listWithAttrs.id, {name: 'A'})}>
+        ${items.map((item) => `<li ${testIdPropString(testIds.listItemWithAttrs(item).id, {status: 'A'})}>${item.name}</li>`)}
+      </ul>
+    `);
+    const ul = dom.window.document.querySelector(testIdSelectors.listWithAttrs.attributes({name: 'A'}));
+    const li1 = dom.window.document.querySelector<HTMLLIElement>(testIdSelectors.listItemWithAttrs(items[0]).attributes({status: 'A'}));
+    const li2 = dom.window.document.querySelector<HTMLLIElement>(testIdSelectors.listItemWithAttrs(items[0]).attributes({status: 'B'}));
+
+    expect(ul).not.toBeNull();
+    expect(li1?.innerHTML).toBe(items[0].name);
+    expect(li2).toBeNull();
   });
 });
--- index.ts
+++ index.ts
@@ -1,25 +1,72 @@
-type StaticId = string;
-type DynamicId = <T>(x: T) => string;
+import {kebabCase, mapKeys} from 'lodash';
 
-type TestIdValue = StaticId | DynamicId;
+export type StaticId = string | WithAttrs;
+export type DynamicId = (x: any) => string | WithAttrs;
 
+export type TestIdValue = StaticId | DynamicId;
 export type TestIds = Record<string, TestIdValue>;
 
-export const makeTestIdSelectors = () => {};
+class WithAttrs<T extends Record<string, any> = Record<string, any>> {
+  constructor(public readonly id: string) {}
+
+  public attributes(attrs: T, attrName?: string): string {
+    return makeSelectorWithAttrs(this.id, attrs, attrName);
+  }
+}
+
+export const withAttrs = <T extends Record<string, any>>(id: string) => new WithAttrs<T>(id);
+
+const testIdSelector = (id: string, attrName = 'test-id'): string => `[data-${attrName}="${id}"]`;
+
+const makeSelectorWithAttrs = (id: string, attrs: Record<string, any>, attrName?: string): string => {
+  return Object.entries(attrs).reduce((acc, [dataAttrName, dataAttrValue]) => {
+    return `${acc}[data-${kebabCase(dataAttrName)}="${dataAttrValue}"]`;
+  }, testIdSelector(id, attrName));
+}; 
+
 export const makeTestIdSelectors = <
-  S extends Record<string, any> = StringResult<string> & CreatorsResult<string>
+  S extends Record<string, TestIdValue>,
 >(
-  testIds: S,
-): S => {
+    testIds: S,
+    attrName?: string,
+  ): S => {
   const selectors: any = {};
 
   for (const key in testIds) {
-    const value: any = testIds[key];
-    selectors[key] =
-      typeof value === 'string'
-        ? testIdSelector(value)
-        : (x: number | string) => testIdSelector(value(x));
+    const value: TestIdValue = testIds[key];
+    
+    if (typeof value === 'string') {
+      selectors[key] = testIdSelector(value, attrName);
+    } else {
+      if (value instanceof WithAttrs) {
+        selectors[key] = value;
+      } else {
+        selectors[key] = (x: any) => {
+          const res = value(x);
+
+          return typeof res === 'string' ? testIdSelector(res) : res;
+        };
+      }
+    }
   }
 
   return selectors;
 };
+
+export const testIdProp = <T extends Record<string, any>, K extends string, S extends string>(
+  id: K,
+  attrs: Record<string, any> = {},
+  attrName: S = ('test-id' as S),
+): T & Record<S, K> => {
+  return mapKeys({
+    [attrName]: id,
+    ...attrs,
+  }, (_, key: string) => `data-${kebabCase(key)}`) as any;
+};
+
+export const testIdPropString = (id: string, attrs: Record<string, any> = {}, attrName = 'test-id'): string => {
+  return Object.entries(attrs).reduce((acc, [key, value]) => {
+    return `${acc} data-${kebabCase(key)}="${value}"`;
+  }, `data-${attrName}="${id}"`);
+};
+
