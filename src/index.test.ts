c413c12881b5703aa7e70f5ce60b74dbb6446323
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2022-06-19 15:57:01 +0400

    prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,10 @@
-import {JSDOM} from 'jsdom';
-import {withAttrs, makeTestIdSelectors, testIdPropString, testIdProp} from './index';
+import { JSDOM } from "jsdom";
+import {
+  withAttrs,
+  makeTestIdSelectors,
+  testIdPropString,
+  testIdProp,
+} from "./index";
 
 type ListItem = {
   id: string;
@@ -7,78 +12,105 @@ type ListItem = {
 };
 
 const testIds = {
-  list: 'list',
-  listWithAttrs: withAttrs<{name: 'A'}>('list'),
+  list: "list",
+  listWithAttrs: withAttrs<{ name: "A" }>("list"),
   listItem: (item: ListItem) => `list-item-${item.id}`,
-  listItemWithAttrs: (item: ListItem) => withAttrs<{status: string}>(`list-item-${item.id}`),
+  listItemWithAttrs: (item: ListItem) =>
+    withAttrs<{ status: string }>(`list-item-${item.id}`),
 };
 
 const testIdSelectors = makeTestIdSelectors(testIds);
-const testIdSelectorsWithAnotherAttrName = makeTestIdSelectors(testIds, 'qa');
+const testIdSelectorsWithAnotherAttrName = makeTestIdSelectors(testIds, "qa");
 
-describe('Test', () => {
+describe("Test", () => {
   const items: ListItem[] = [
     {
-      name: 'first',
-      id: '1',
+      name: "first",
+      id: "1",
     },
     {
-      name: 'second',
-      id: '2',
+      name: "second",
+      id: "2",
     },
   ];
 
-  test('static field', () => {
+  test("static field", () => {
     expect(testIdSelectors.list).toBe('[data-test-id="list"]');
     expect(testIdSelectorsWithAnotherAttrName.list).toBe('[data-qa="list"]');
   });
 
-  test('dynamic field', () => {
-    expect(testIdSelectors.listItem(items[0])).toBe('[data-test-id="list-item-1"]');
-    expect(testIdSelectorsWithAnotherAttrName.listItem(items[0])).toBe('[data-qa="list-item-1"]');
+  test("dynamic field", () => {
+    expect(testIdSelectors.listItem(items[0])).toBe(
+      '[data-test-id="list-item-1"]'
+    );
+    expect(testIdSelectorsWithAnotherAttrName.listItem(items[0])).toBe(
+      '[data-qa="list-item-1"]'
+    );
   });
 
-  test('static field with attrs', () => {
-    expect(testIdSelectors.listWithAttrs({name: 'A'})).toBe('[data-test-id="list"][data-name="A"]');
+  test("static field with attrs", () => {
+    expect(testIdSelectors.listWithAttrs({ name: "A" })).toBe(
+      '[data-test-id="list"][data-name="A"]'
+    );
   });
 
-  test('dynamic field with attrs', () => {
-    expect(testIdSelectors.listItemWithAttrs(items[0], {status: 'A'})).toBe('[data-test-id="list-item-1"][data-status="A"]');
+  test("dynamic field with attrs", () => {
+    expect(testIdSelectors.listItemWithAttrs(items[0], { status: "A" })).toBe(
+      '[data-test-id="list-item-1"][data-status="A"]'
+    );
   });
 
-  test('testIdProp', () => {
-    expect(testIdProp(testIds.list, {name: 'A'})).toEqual({
-      'data-test-id': testIds.list,
-      'data-name': 'A',
+  test("testIdProp", () => {
+    expect(testIdProp(testIds.list, { name: "A" })).toEqual({
+      "data-test-id": testIds.list,
+      "data-name": "A",
     });
   });
 
-  test('testIdPropString', () => {
-    expect(testIdPropString(testIds.listItem(items[0]), {status: 'A'})).toBe(`data-test-id="${testIds.listItem(items[0])}" data-status="A"`);
+  test("testIdPropString", () => {
+    expect(testIdPropString(testIds.listItem(items[0]), { status: "A" })).toBe(
+      `data-test-id="${testIds.listItem(items[0])}" data-status="A"`
+    );
   });
 
-  test('jsdom', () => {
+  test("jsdom", () => {
     const dom = new JSDOM(`
       <ul ${testIdPropString(testIds.list)}>
-        ${items.map((item) => `<li ${testIdPropString(testIds.listItem(item))}>${item.name}</li>`)}
+        ${items.map(
+          (item) =>
+            `<li ${testIdPropString(testIds.listItem(item))}>${item.name}</li>`
+        )}
       </ul>
     `);
     const ul = dom.window.document.querySelector(testIdSelectors.list);
-    const li1 = dom.window.document.querySelector<HTMLLIElement>(testIdSelectors.listItem(items[0]));
+    const li1 = dom.window.document.querySelector<HTMLLIElement>(
+      testIdSelectors.listItem(items[0])
+    );
 
     expect(ul).not.toBeNull();
     expect(li1?.innerHTML).toBe(items[0].name);
   });
 
-  test('jsdom with attributes', () => {
+  test("jsdom with attributes", () => {
     const dom = new JSDOM(`
-      <ul ${testIdPropString(testIds.listWithAttrs as string, {name: 'A'})}>
-        ${items.map((item) => `<li ${testIdPropString(testIds.listItemWithAttrs(item) as string, {status: 'A'})}>${item.name}</li>`)}
+      <ul ${testIdPropString(testIds.listWithAttrs as string, { name: "A" })}>
+        ${items.map(
+          (item) =>
+            `<li ${testIdPropString(testIds.listItemWithAttrs(item) as string, {
+              status: "A",
+            })}>${item.name}</li>`
+        )}
       </ul>
     `);
-    const ul = dom.window.document.querySelector(testIdSelectors.listWithAttrs({name: 'A'}));
-    const li1 = dom.window.document.querySelector<HTMLLIElement>(testIdSelectors.listItemWithAttrs(items[0], {status: 'A'}));
-    const li2 = dom.window.document.querySelector<HTMLLIElement>(testIdSelectors.listItemWithAttrs(items[0], {status: 'B'}));
+    const ul = dom.window.document.querySelector(
+      testIdSelectors.listWithAttrs({ name: "A" })
+    );
+    const li1 = dom.window.document.querySelector<HTMLLIElement>(
+      testIdSelectors.listItemWithAttrs(items[0], { status: "A" })
+    );
+    const li2 = dom.window.document.querySelector<HTMLLIElement>(
+      testIdSelectors.listItemWithAttrs(items[0], { status: "B" })
+    );
 
     expect(ul).not.toBeNull();
     expect(li1?.innerHTML).toBe(items[0].name);
--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,113 @@
-import {kebabCase, mapKeys, mapValues} from 'lodash';
-import {StaticId, StaticIdWithAttrs, DynamicId, DynamicIdWithAttrs, TestId, WithAttrsString} from './types';
+import { kebabCase, mapKeys, mapValues } from "lodash";
+import {
+  StaticId,
+  StaticIdWithAttrs,
+  DynamicId,
+  DynamicIdWithAttrs,
+  TestId,
+  WithAttrsString,
+} from "./types";
 
-export function withAttrs<T extends Record<string, any>>(x: string): StaticIdWithAttrs<T> {return new WithAttrsString(x)}
+export function withAttrs<T extends Record<string, any>>(
+  x: string
+): StaticIdWithAttrs<T> {
+  return new WithAttrsString(x);
+}
 
-// eslint-disable-next-line
-const testIdSelector = (id: String, attrName = 'test-id'): string =>
+const testIdSelector = (id: string, attrName = "test-id"): string =>
   `[data-${attrName}="${id}"]`;
 
 const makeSelectorWithAttrs = (
-// eslint-disable-next-line
+  // eslint-disable-next-line
   id: String, attrs: Record<string, any>,
   attrName?: string
 ): string => {
   return Object.entries(attrs).reduce((acc, [dataAttrName, dataAttrValue]) => {
     return `${acc}[data-${kebabCase(dataAttrName)}="${dataAttrValue}"]`;
-  }, testIdSelector(id, attrName));
+  }, testIdSelector(id as string, attrName));
 };
 
-const isStaticId = (x: TestId): x is StaticId => !(x instanceof WithAttrsString) && typeof x === 'string';
-const isStaticIdWithAttrs = (x: TestId): x is StaticIdWithAttrs => x instanceof WithAttrsString;
-const isDynamicId = (x: DynamicId | DynamicIdWithAttrs, id: StaticId | StaticIdWithAttrs): x is DynamicId => {
-  return isStaticId(id); 
+const isStaticId = (x: TestId): x is StaticId =>
+  !(x instanceof WithAttrsString) && typeof x === "string";
+const isStaticIdWithAttrs = (x: TestId): x is StaticIdWithAttrs =>
+  x instanceof WithAttrsString;
+const isDynamicId = (
+  x: DynamicId | DynamicIdWithAttrs,
+  id: StaticId | StaticIdWithAttrs
+): x is DynamicId => {
+  return isStaticId(id);
 };
 
 type TestIdSelectors<T extends Record<string, TestId>> = {
   [P in keyof T]: T[P] extends StaticId
     ? T[P]
-    : (T[P] extends DynamicId
-      ? T[P]
-      : (T[P] extends StaticIdWithAttrs<infer A>
-        ? ((attrs: A) => string)
-        : T[P] extends (x: any) => StaticIdWithAttrs<infer A>
-          ? ((x: Parameters<T[P]>[0], attrs: A) => string)
-          : never))
+    : T[P] extends DynamicId
+    ? T[P]
+    : T[P] extends StaticIdWithAttrs<infer A>
+    ? (attrs: A) => string
+    : T[P] extends (x: any) => StaticIdWithAttrs<infer A>
+    ? (x: Parameters<T[P]>[0], attrs: A) => string
+    : never;
 };
 
-export const makeTestIdSelectors = <
-  S extends Record<string, TestId>,
->(
-    testIds: S,
-    attrName?: string,
-  ): TestIdSelectors<S> => {
-  return mapValues(testIds as any, (value: TestId) => { 
+export const makeTestIdSelectors = <S extends Record<string, TestId>>(
+  testIds: S,
+  attrName?: string
+): TestIdSelectors<S> => {
+  return mapValues(testIds as any, (value: TestId) => {
     if (isStaticId(value)) {
       return testIdSelector(value, attrName);
     } else if (isStaticIdWithAttrs(value)) {
-      return (attrs: Record<string, any>) => makeSelectorWithAttrs(value, attrs, attrName);
+      return (attrs: Record<string, any>) =>
+        makeSelectorWithAttrs(value, attrs, attrName);
     } else {
       return (x: any, attrs: Record<string, any> = {}) => {
         const res = value(x);
-        return isDynamicId(value, res) ? testIdSelector(res, attrName) : makeSelectorWithAttrs(res, attrs, attrName);
+        return isDynamicId(value, res)
+          ? testIdSelector(res as string, attrName)
+          : makeSelectorWithAttrs(res, attrs, attrName);
       };
     }
   }) as any;
 };
 
-export function testIdProp<T extends StaticId>(id: T, attrName?: string): Record<string, string>;
-export function testIdProp<T extends StaticIdWithAttrs, A extends T['__attrs']>(
+export function testIdProp<T extends StaticId>(
+  id: T,
+  attrName?: string
+): Record<string, string>;
+export function testIdProp<T extends StaticIdWithAttrs, A extends T["__attrs"]>(
   id: T,
   attrs: A,
   attrName?: string
 ): Record<string, string>;
-export function testIdProp(id: string, attrs: any = {}, attrName = 'test-id'): any {
-  return mapKeys({
-    [attrName]: id.valueOf(),
-    ...attrs,
-  }, (_, key: string) => `data-${kebabCase(key)}`);
+export function testIdProp(
+  id: string,
+  attrs: any = {},
+  attrName = "test-id"
+): any {
+  return mapKeys(
+    {
+      [attrName]: id.valueOf(),
+      ...attrs,
+    },
+    (_, key: string) => `data-${kebabCase(key)}`
+  );
 }
 
-export function testIdPropString<T extends StaticId>(id: T, attrName?: string): string;
-export function testIdPropString<T extends StaticIdWithAttrs, A extends T['__attrs']>(id: T, attrs: A, attrName?: string): string;
-export function testIdPropString(id: string, attrs: any = {}, attrName = 'test-id'): string {
+export function testIdPropString<T extends StaticId>(
+  id: T,
+  attrName?: string
+): string;
+export function testIdPropString<
+  T extends StaticIdWithAttrs,
+  A extends T["__attrs"]
+>(id: T, attrs: A, attrName?: string): string;
+export function testIdPropString(
+  id: string,
+  attrs: any = {},
+  attrName = "test-id"
+): string {
   return Object.entries(attrs).reduce((acc, [key, value]) => {
     return `${acc} data-${kebabCase(key)}="${value}"`;
   }, `data-${attrName}="${id}"`);
 }
-
